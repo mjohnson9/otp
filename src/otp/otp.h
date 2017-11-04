c5f1dab8dda0d487a3f5e8a68bc34381980bc697
Michael Johnson <michael@johnson.computer>
2017-11-04 08:10:37 -0400

    Actually export the otp functions

--- src/otp/otp.h
+++ src/otp/otp.h
@@ -2,12 +2,14 @@
 
 #include <vector>
 
+#include "otp_macros.h"
+
 namespace otp {
 
-const unsigned char MIN = 'A';
-const unsigned char MAX = 'Z';
+OTP_EXPORT const unsigned char MIN = 'A';
+OTP_EXPORT const unsigned char MAX = 'Z';
 
-unsigned char encrypt(unsigned char plaintext, unsigned char key);
-unsigned char decrypt(unsigned char ciphertext, unsigned char key);
+OTP_EXPORT unsigned char encrypt(unsigned char plaintext, unsigned char key);
+OTP_EXPORT unsigned char decrypt(unsigned char ciphertext, unsigned char key);
 
 }  // namespace otp
---
+++ src/otp/otp_macros.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define OTP_EXPORT __attribute__((visibility("default"))) extern
