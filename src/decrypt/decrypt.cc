b7c88060c7a09135c31e7fe40afefdd4e9d08064
Michael Johnson <michael@johnson.computer>
2017-11-04 09:33:14 -0400

    googlelog: not needed

--- src/decrypt/decrypt.cc
+++ src/decrypt/decrypt.cc
@@ -2,12 +2,4 @@
 #include <iostream>
 #include <string>
 
-#include "glog/logging.h"
-
-int main(int argc, char* argv[]) {
-  google::InitGoogleLogging(argv[0]);
-
-  LOG(INFO) << "Test";
-
-  return 0;
-}
+int main(int argc, char* argv[]) { return 0; }
