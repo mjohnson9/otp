521b4cbbb6cc42e3aadf6c83b35db88620652d2d
Michael Johnson <michael@johnson.computer>
2017-11-08 11:28:21 -0500

    Remove commented non-tests

--- src/otp/otp_test.cc
+++ src/otp/otp_test.cc
@@ -6,23 +6,6 @@
 
 namespace otp {
 
-/*TEST(Factor, SmallNumbers) {
-  EXPECT_EQ(factor(1), std::vector<int64_t>({1}));
-  EXPECT_EQ(factor(2), std::vector<int64_t>({2}));
-  EXPECT_EQ(factor(3), std::vector<int64_t>({3}));
-  EXPECT_EQ(factor(4), std::vector<int64_t>({2, 2}));
-  EXPECT_EQ(factor(5), std::vector<int64_t>({5}));
-  EXPECT_EQ(factor(6), std::vector<int64_t>({2, 3}));
-  EXPECT_EQ(factor(7), std::vector<int64_t>({7}));
-  EXPECT_EQ(factor(8), std::vector<int64_t>({2, 2, 2}));
-  EXPECT_EQ(factor(9), std::vector<int64_t>({3, 3}));
-}
-
-TEST(Factor, BigNumbers) {
-  EXPECT_EQ(factor(1209), std::vector<int64_t>({3, 13, 31}));
-  EXPECT_EQ(factor(4242), std::vector<int64_t>({2, 3, 7, 101}));
-}*/
-
 TEST(otp, reciprocity) {
   for (unsigned char key = MIN; key <= MAX; key++) {
     for (unsigned char plaintext = MIN; key <= MAX; key++) {
