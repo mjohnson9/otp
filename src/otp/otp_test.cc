5a2453cfa900247ae55ac3a14f2096032f4fdcb2
Michael Johnson <michael@johnson.computer>
2017-11-08 11:28:34 -0500

    Test for valid output from encrypt and decrypt

--- src/otp/otp_test.cc
+++ src/otp/otp_test.cc
@@ -14,6 +14,26 @@ TEST(otp, reciprocity) {
   }
 }
 
+TEST(encrypt, valid_output) {
+  for (unsigned char key = MIN; key <= MAX; key++) {
+    for (unsigned char plaintext = MIN; key <= MAX; key++) {
+      unsigned char ciphertext = encrypt(plaintext, key);
+      EXPECT_GE(ciphertext, otp::MIN);
+      EXPECT_LE(ciphertext, otp::MAX);
+    }
+  }
+}
+
+TEST(decrypt, valid_output) {
+  for (unsigned char key = MIN; key <= MAX; key++) {
+    for (unsigned char ciphertext = MIN; key <= MAX; key++) {
+      unsigned char plaintext = decrypt(ciphertext, key);
+      EXPECT_GE(plaintext, otp::MIN);
+      EXPECT_LE(plaintext, otp::MAX);
+    }
+  }
+}
+
 }  // namespace otp
 
 int main(int argc, char** argv) {
