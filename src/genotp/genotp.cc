33bf5191630db47a48dbf97d93709330a22e47a7
Michael Johnson <michael@johnson.computer>
2017-11-08 11:02:42 -0500

    Add googleflags

--- src/genotp/genotp.cc
+++ src/genotp/genotp.cc
@@ -1,6 +1,12 @@
 #include <cstdint>
 #include <iostream>
 
+#include <gflags/gflags.h>
+
 #include "otp/otp.h"
 
-int main() { return 0; }
+int main(int argc, char* argv[]) {
+  gflags::SetUsageMessage("generate OTP key material");
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  return 0;
+}
