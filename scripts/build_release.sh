c770b76437c4ce7e83295312f1fc7168183aae89
Michael Johnson <michael@johnson.computer>
2019-07-07 10:48:12 -0400

    Post-semester addition of build script

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 main() {
 	export CC="clang"
 	export CXX="clang++"
-	cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -Hsrc -Bbuild
+	cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -Hsrc -Bbuild "$@"
 	exec cmake --build build
 }
 
-main
+main "$@"
